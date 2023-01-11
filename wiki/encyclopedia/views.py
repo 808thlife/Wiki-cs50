fa86948527967d94635f81db12fe510c6f5ccafd
808thlife <khqyop@gmail.com>
2023-01-11 18:22:10 +0500

    Implemented edit function

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django import forms
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from . import util
 from markdown2 import Markdown
 import os
@@ -23,7 +23,6 @@ def index(request):
         "entries": util.list_entries()
     })
 
-
 def new_page(request):
     form = NewPage(request.POST)
     if form.is_valid():
@@ -37,6 +36,7 @@ def new_page(request):
     )
 
     return render(request, './encyclopedia/newpage.html', {'form': form})
+
 def entry(request,title):
     markdowner = Markdown()
     content = util.get_entry(title)
@@ -50,15 +50,23 @@ def entry(request,title):
         })
 
 def edit(request,title):
-   
+    content = util.get_entry(title)
     form = EditPage(request.POST)
+    initial = EditPage(initial={'content': content, 'title':title})
     if form.is_valid():
-        f = open('entries/' + form.cleaned_data['title'] + '.md', 'x')
-        f.write(form.cleaned_data['content'])
-        f.close()
-        return redirect('index')
+        content = form.cleaned_data['content']
+        util.save_entry(title, content)
+        return redirect(f'wiki/{title}')
+    else:
+        content = util.get_entry(title)
+        form = EditPage(initial= {'title':title, 'content':content})
+
     return render(request, './encyclopedia/edit.html', {
+        'content': content,
         'title':title,
-        'form': form
+        'form': form,     
     }
     )
+
+
+
